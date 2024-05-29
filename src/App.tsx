81d2b13d9fecddcd9b57ee279d72d592b8c6f0ff
Justin Niessner <CrshOverride@gmail.com>
2024-05-29 10:46:07 -0400

    Add geocoding w/ console log of results

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
 
-import { APIProvider, ControlPosition, Map, MapControl } from '@vis.gl/react-google-maps'
+import { APIProvider, ControlPosition, Map, MapControl, useMapsLibrary } from '@vis.gl/react-google-maps'
 import { useDrawingManager } from './useDrawingManager'
 import { UndoRedoControl } from './UndoRedoControl'
 
 function App() {
-  const onMark = (lat: number, long: number) => {
+  let geocoder = undefined;
+  const geocoding = useMapsLibrary("geocoding");
+  if (geocoding) {
+    geocoder = new geocoding.Geocoder();
+  }
+  
+  const onMark = async (lat: number, long: number) => {
     console.log(`Lat: ${lat}, Long: ${long}`);
+    if (!geocoder) {
+      return;
+    }
+    
+    var address = await geocoder.geocode({ location: { lat, lng: long }, language: "en-US" });
+    console.log(address);
   };
 
   const drawingManager = useDrawingManager(null, onMark);
